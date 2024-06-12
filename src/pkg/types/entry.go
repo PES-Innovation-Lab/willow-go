16af304352ad81e58718b0c22d5e52884ccaaa51
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2024-06-12 18:13:15 +0530

    Entry struct modified

--- src/pkg/types/entry.go
+++ src/pkg/types/entry.go
@@ -1,17 +1,19 @@
 // Types related to Entry in the data model
 package types
 
-type Entry[Timestamp uint64, NamespaceId, SubspaceId, PayloadDigest any] struct {
+import "golang.org/x/exp/constraints"
+
+type Entry[NamespaceId, SubspaceId, PayloadDigest constraints.Ordered] struct {
 	// ID of the namespace the Entry is a part of
 	namespace_id NamespaceId
 	// ID of the subspace to which the Entry belongs to
 	subspace_id SubspaceId
 	// The path which the entry has
 	path Path
-	// The time at which the entry was created in microseconds
-	timestamp Timestamp
-	// The length of the payload
-	payload_length uint64
 	// The hashed payload
 	payload_digest PayloadDigest
+	// The time at which the entry was created in microseconds
+	timestamp uint64
+	// The length of the payload
+	payload_length uint64
 }
