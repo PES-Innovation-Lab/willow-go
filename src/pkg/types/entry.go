ed51b629fa0f063d4dc1a7ad11fe95a043caa7d6
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2024-06-12 16:17:39 +0530

    Added the Entry type

---
+++ src/pkg/types/entry.go
@@ -0,0 +1,17 @@
+// Types related to Entry in the data model
+package types
+
+type Entry struct {
+	// ID of the namespace the Entry is a part of
+	namespace_id NamespaceId
+	// ID of the subspace to which the Entry belongs to
+	subspace_id SubspaceId
+	// The path which the entry has
+	path Path
+	// The time at which the entry was created in microseconds
+	timestamp Timestamp
+	// The length of the payload
+	payload_length uint64
+	// The hashed payload
+	payload_digest PayloadDigest
+}
