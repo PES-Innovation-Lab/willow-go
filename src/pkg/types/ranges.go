2e6eeea43559ff44b998973043a5fed2e8b04103
Samsonnyyeet <aries.samar@gmail.com>
2024-06-12 16:49:52 +0530

    ranges type added

---
+++ src/pkg/types/ranges.go
@@ -0,0 +1,16 @@
+package types
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Range[T constraints.Ordered] struct {
+	Start T
+	End   *T // Use pointer to indicate open range if nil
+}
+
+type Range3D[SubspaceId constraints.Ordered] struct {
+	Subspaces Range[SubspaceId]
+	Paths     Range[Path]
+	Times     Range[uint64]
+}
