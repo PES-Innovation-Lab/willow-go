ab8f1852661c773eb38048d7ad04a9eb99ac0d71
Samsonnyyeet <aries.samar@gmail.com>
2024-06-13 11:28:30 +0530

    added area types and aoi types

--- src/pkg/types/areas.go
+++ src/pkg/types/areas.go
@@ -1,4 +1,17 @@
 package types
 
-// TODO Depends on Range
+import (
+	"golang.org/x/exp/constraints"
+)
 
+type Area[SubspaceId constraints.Ordered] struct {
+	subspace_id SubspaceId
+	path        Path
+	times       Range[uint64]
+}
+
+type AreaOfInterest[SubspaceId constraints.Ordered] struct {
+	area      Area[SubspaceId]
+	max_count uint64
+	max_size  uint64
+}
