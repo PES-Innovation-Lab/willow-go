557c546e9309b6fd0d35cd1c7d262c07f5099da1
samarth-clone <samarthbhaty@gmail.com>
2024-06-13 14:03:10 +0530

    Encoding JOVER, yet to test :(

--- src/pkg/utils/encoding.go
+++ src/pkg/utils/encoding.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"math/big"
 
 	"golang.org/x/exp/constraints"
@@ -26,8 +28,8 @@ func GetWidthMax32Int[T constraints.Unsigned](num T) int {
 	}
 }
 
-func EncodingIntMax32[T constraints.Unsigned](num, max T) []byte {
-	width := GetWidthMax32Int(num)
+func EncodeIntMax32[T constraints.Unsigned](num, max T) []byte {
+	width := GetWidthMax32Int(max)
 	bytes := make([]byte, width)
 
 	switch width {
@@ -45,6 +47,28 @@ func EncodingIntMax32[T constraints.Unsigned](num, max T) []byte {
 	return bytes
 }
 
+func DecodeIntMax32[T constraints.Unsigned](bytes []byte, max uint32) (uint32, error) {
+	bytesToDecodeLength := GetWidthMax32Int(max)
+
+	if bytesToDecodeLength > 4 {
+		return 0, errors.New("Cannot decode non-UintMax bytes")
+	}
+
+	if bytesToDecodeLength == 1 {
+		return uint32(bytes[0]), nil
+	} else if bytesToDecodeLength == 2 {
+		return uint32(binary.BigEndian.Uint16(bytes)), nil
+	} else if bytesToDecodeLength == 4 {
+		return binary.BigEndian.Uint32(bytes), nil
+	}
+
+	// Otherwise it's 24 bit.
+	a := binary.BigEndian.Uint16(bytes[:2])
+	b := uint32(bytes[2])
+
+	return (uint32(a) << 8) + b, nil
+}
+
 func GetWidthMax64Int[T constraints.Unsigned](num T) int {
 	switch true {
 	case int(num) < 1<<8:
@@ -58,8 +82,8 @@ func GetWidthMax64Int[T constraints.Unsigned](num T) int {
 	}
 }
 
-func EncodingIntMax64[T constraints.Unsigned](num, max T) []byte {
-	width := GetWidthMax64Int(num)
+func EncodeIntMax64[T constraints.Unsigned](num, max T) []byte {
+	width := GetWidthMax64Int(max)
 	bytes := make([]byte, width)
 
 	switch width {
@@ -76,4 +100,26 @@ func EncodingIntMax64[T constraints.Unsigned](num, max T) []byte {
 	return bytes
 }
 
-func DecodeCompactWidth(encoded []bytes)
+func DecodeIntMax64[T constraints.Unsigned](encoded []byte, max uint32) any {
+	reader := bytes.NewReader(encoded)
+
+	switch len(encoded) {
+	case 1:
+		var val uint8
+		binary.Read(reader, binary.BigEndian, &val)
+		return val
+	case 2:
+		var val uint16
+		binary.Read(reader, binary.BigEndian, &val)
+		return val
+	case 4:
+		var val uint32
+		binary.Read(reader, binary.BigEndian, &val)
+		return val
+	default:
+		var val uint64
+		binary.Read(reader, binary.BigEndian, &val)
+		return val
+	}
+
+}
