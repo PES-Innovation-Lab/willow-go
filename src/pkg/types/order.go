d89f629e8cdcae9877e1b09cc213c8425d910f27
suyog301103 <117434229+suyog301103@users.noreply.github.com>
2024-06-14 11:01:59 +0530

    Making changes

--- src/pkg/types/order.go
+++ src/pkg/types/order.go
@@ -1,9 +1,25 @@
 package types
 
-import "golang.org/x/exp/constraints"
+import (
+	"cmp"
+
+	"golang.org/x/exp/constraints"
+)
+
+type Rel int
+
+type OrderableGeneric interface {
+	cmp.Ordered | Path
+}
+
+const (
+	Less    Rel = -1
+	Equal   Rel = 0
+	Greater Rel = 1
+)
 
 // TotalOrder defines a total order over a given set.
-type TotalOrder func(a, b interface{}) int
+type TotalOrder[T OrderableGeneric] func(a, b T) Rel
 
 // SuccessorFn returns the succeeding value for a given value of a set.
 type SuccessorFn[T constraints.Ordered] func(val T) *T
