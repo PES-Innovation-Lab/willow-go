56a5a8e2ca366ce7e032efc0abfc83307678a18c
Samsonnyyeet <aries.samar@gmail.com>
2024-06-14 12:24:43 +0530

    more addtions to ranges

--- src/pkg/utils/ranges.go
+++ src/pkg/utils/ranges.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/PES-Innovation-Lab/willow-go/src/pkg/types"
-	"golang.org/x/exp/constraints"
 )
 
 // Constants for open end and closed end representations
@@ -10,9 +11,9 @@ var OPEN_END = new(interface{})
 
 // orderRangePair orders two Range structs based on their end values.
 func OrderRangePair[T types.OrderableGeneric](a, b types.Range[T]) (types.Range[T], types.Range[T]) {
-	if (a.End == nil && b.End == nil) ||
-		(a.End != nil && b.End != nil) ||
-		(a.End == nil && b.End != nil) {
+	if (!a.OpenEnd && !b.OpenEnd) ||
+		(a.OpenEnd && b.OpenEnd) ||
+		(a.OpenEnd && !b.OpenEnd) {
 		return a, b
 	}
 	return b, a
@@ -37,33 +38,74 @@ func IsIncludedRange[T types.OrderableGeneric](order types.TotalOrder[T], r type
 	return ltEnd
 }
 
-func IntersectRange[T types.OrderableGeneric](order types.TotalOrder[T], a, b types.Range[T]) types.Range[T] {
-	
+func IntersectRange[T types.OrderableGeneric](order types.TotalOrder[T], a, b types.Range[T]) (bool, types.Range[T]) {
+
 	if !IsValidRange(order, a) || !IsValidRange(order, b) {
 		fmt.Println("Paths are not valid paths... BOZO CAN'T EVEN PASS PATHS PROPERLY AHH")
 	}
 
-	a, b :=  OrderRangePair(a, b)
-	if a.OpenEnd && b.OpenEnd {
-		return types.Range[T] {
-			Start: func() T { if order(a.Start, b.Start) <= 0 { return b.Start } else { return a.Start } }(),
-			End: 0,
-			OpenEnd: true,
-		}
-	} else if a.OpenEnd && !b.OpenEnd {
-		aStartbStartOrder := order(a.Start, b.Start)
-		aStartbEndOrder := order(a.Start, b.Start)
+	a, b = OrderRangePair(a, b)
 
-		if aStartbStartOrder <= 0 {
-			return b
-		} else if aStartbStartOrder > 0 && aStartbEndOrder < 0 {
-			return types.Range[T] {
-				Start: a.Start,
-				End: b.End,
-			}
+	// case when both ranges are open-ended
+	if a.OpenEnd && b.OpenEnd {
+		start := a.Start
+		if order(a.Start, b.Start) <= 0 {
+			start = b.Start
+		}
+		return true, types.Range[T]{
+			Start:   start,
+			OpenEnd: true,
 		}
 	}
 
+	// case when only 'a' is open-ended
+	if a.OpenEnd && !b.OpenEnd {
+		aStartBStartOrder := order(a.Start, b.Start)
+		aStartBEndOrder := order(a.Start, b.End)
+
+		if aStartBStartOrder <= 0 {
+			return true, b
+		} else if aStartBStartOrder > 0 && aStartBEndOrder < 0 {
+			return true, types.Range[T]{
+				Start: a.Start,
+				End:   b.End,
+			}
+		}
+
+		return false, types.Range[T]{}
+	}
+
+	// Case when both ranges are closed-ended
+	if !a.OpenEnd && !b.OpenEnd {
+		min := a
+		max := b
+		if order(a.Start, b.Start) >= 0 {
+			min, max = b, a
+		}
+
+		// reject if min's end is less than or equal to max's start
+		if order(min.End, max.Start) <= 0 {
+			return false, types.Range[T]{}
+		}
+
+		// reject if max's start is greater than or equal to min's end
+		if order(max.Start, min.End) >= 0 {
+			return false, types.Range[T]{}
+		}
+
+		return true, types.Range[T]{
+			Start: max.Start,
+			End: func() T {
+				if order(min.End, max.End) < 0 {
+					return min.End
+				}
+				return max.End
+			}(),
+		}
+	}
+
+	return false, types.Range[T]{}
+}
 
 // 	x, y := OrderRangePair(a, b)
 
