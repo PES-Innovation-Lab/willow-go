dcda17141b22375b7d3cff32129e560222a0277c
Achyuth <achyuthyogesh0@gmail.com>
2024-06-20 10:52:36 +0530

    feat: Entry utils

--- utils/entries.go
+++ utils/entries.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/binary"
+	"fmt"
 	"math"
 
 	types "github.com/PES-Innovation-Lab/willow-go/types"
@@ -47,6 +48,10 @@ func EncodeEntry[NamespaceKey, SubspaceKey, PayloadDigest constraints.Ordered, V
 
 }
 
+/** Decode bytes to an `Entry`.
+ *
+ * https://willowprotocol.org/specs/encodings/index.html#enc_entry
+ */
 func DecodeEntry[NamespaceKey, SubspaceKey, PayloadDigest constraints.Ordered, ValueType constraints.Unsigned](
 	opts struct {
 		NameSpaceScheme EncodingScheme[NamespaceKey, ValueType]
@@ -55,19 +60,19 @@ func DecodeEntry[NamespaceKey, SubspaceKey, PayloadDigest constraints.Ordered, V
 		PathScheme      types.PathParams[ValueType]
 	},
 	encEntry []byte,
-) types.Entry[NamespaceKey, SubspaceKey, PayloadDigest] {
-
+) (types.Entry[NamespaceKey, SubspaceKey, PayloadDigest], error) {
+	// first get the namespace.
 	namespaceId, err := opts.NameSpaceScheme.Decode(encEntry)
 	if err != nil {
-		panic(err)
+		return types.Entry[NamespaceKey, SubspaceKey, PayloadDigest]{}, fmt.Errorf("failed to decode namespace: %w", err)
 	}
-
+	// now get the the subSpace after finding starting position
 	subspacePos := opts.NameSpaceScheme.EncodedLength(namespaceId)
 	subspaceId, err := opts.SubSpaceScheme.Decode(encEntry[subspacePos:])
 	if err != nil {
-		panic(err)
+		return types.Entry[NamespaceKey, SubspaceKey, PayloadDigest]{}, fmt.Errorf("failed to decode subspace: %w", err)
 	}
-
+	// Similar approach for Path but decoded as a stream instead
 	pathPos := subspacePos + opts.SubSpaceScheme.EncodedLength(subspaceId)
 
 	pathStream := make(chan []byte, 10)
@@ -82,19 +87,23 @@ func DecodeEntry[NamespaceKey, SubspaceKey, PayloadDigest constraints.Ordered, V
 		}
 	}()
 	path := DecodePathStream(opts.PathScheme, pathBytes)
+	// now get the timestamp
 
 	timestampPos := pathPos + EncodePathLength(opts.PathScheme, path)
 	timestamp := binary.BigEndian.Uint64(encEntry[timestampPos:])
 
+	// timestamp takes up 8 bytes
+
 	payloadLengthPos := timestampPos + 8
 	payloadLength := binary.BigEndian.Uint64(encEntry[payloadLengthPos:])
+	// payload digest takes up 8 bytes
 	payloadDigestPos := payloadLength + 8
 
 	payloadDigest, err := opts.PayloadScheme.Decode(encEntry[payloadDigestPos:])
 	if err != nil {
-		panic(err)
+		return types.Entry[NamespaceKey, SubspaceKey, PayloadDigest]{}, fmt.Errorf("failed to decode payloaddigest: %w", err)
 	}
-
+	// decoded entry
 	return types.Entry[NamespaceKey, SubspaceKey, PayloadDigest]{
 		Namespace_id:   namespaceId,
 		Subspace_id:    subspaceId,
@@ -102,8 +111,10 @@ func DecodeEntry[NamespaceKey, SubspaceKey, PayloadDigest constraints.Ordered, V
 		Timestamp:      timestamp,
 		Payload_length: payloadLength,
 		Payload_digest: payloadDigest,
-	}
+	}, nil
 }
+
+/* Encode entry relative to another entry */
 
 func EncodeEntryRelativeEntry[NamespaceId, SubspaceId, PayloadDigest constraints.Ordered, ValueType constraints.Unsigned](
 	opts struct {
@@ -116,33 +127,38 @@ func EncodeEntryRelativeEntry[NamespaceId, SubspaceId, PayloadDigest constraints
 	}, entry types.Entry[NamespaceId, SubspaceId, PayloadDigest],
 	ref types.Entry[NamespaceId, SubspaceId, PayloadDigest],
 ) []byte {
+
+	// Time difference
 	timeDiff := AbsDiffuint64(entry.Timestamp, ref.Timestamp)
+
 	var encodedNamespaceFlag int
+	/* Are namespaces equal or not? Does it need to be encoded? */
 	if !opts.IsEqualNamespace(entry.Namespace_id, ref.Namespace_id) {
 		encodedNamespaceFlag = 0x80
 	} else {
 		encodedNamespaceFlag = 0x00
 	}
 	var encodedSubspaceFlag int
-
+	/* Does subspace need to be encoded */
 	if opts.OrderSubspace(entry.Subspace_id, ref.Subspace_id) != 0 {
 		encodedSubspaceFlag = 0x40
 	} else {
 		encodedSubspaceFlag = 0x0
 	}
 	var addOrSubtractTimeDiff int
-
+	// Add or subtract
 	if entry.Timestamp-ref.Timestamp > 0 {
 		addOrSubtractTimeDiff = 0x20
 	} else {
 		addOrSubtractTimeDiff = 0x0
 	}
-
+	// 2-bit integer n such that 2^n gives compact_width(time_diff)
 	compactWidthTimeDiffFlag := CompactWidthEndMasks[GetWidthMax64Int(timeDiff)] << 2
+	// 2-bit integer n such that 2^n gives compact_width(e.payload_length)
 	compactWidthPayloadLengthFlag := CompactWidthEndMasks[GetWidthMax64Int(entry.Payload_length)]
 
 	var encodedNamespace []byte
-
+	// Encoded namespace
 	if encodedNamespaceFlag == 0x0 {
 		encodedNamespace = []byte{}
 	} else {
@@ -150,7 +166,7 @@ func EncodeEntryRelativeEntry[NamespaceId, SubspaceId, PayloadDigest constraints
 	}
 
 	var encodedSubspace []byte
-
+	// Encoded subspace
 	if encodedSubspaceFlag == 0x0 {
 		encodedSubspace = []byte{}
 	} else {
@@ -158,9 +174,11 @@ func EncodeEntryRelativeEntry[NamespaceId, SubspaceId, PayloadDigest constraints
 	}
 
 	encodedPath := EncodeRelativePath[ValueType](opts.PathScheme, entry.Path, ref.Path)
-
+	// time_diff, encoded as an unsigned, big-endian compact_width(time_diff)-byte integer
 	encodedTimeDiff := EncodeIntMax64(timeDiff)
+	//e.payload_length, encoded as an unsigned, big-endian compact_width(e.payload_length)-byte integer
 	encodedPayloadLength := EncodeIntMax64(entry.Payload_length)
+	// e.payload_digest, encoded as a payload_digest
 	encodedDigest := opts.EncodePayloadDigest(entry.Payload_digest)
 
 	header := encodedNamespaceFlag | encodedSubspaceFlag | addOrSubtractTimeDiff | compactWidthPayloadLengthFlag | compactWidthTimeDiffFlag
@@ -185,6 +203,8 @@ var CompactWidthEndMasks = map[int]int{
 	4: 0x2,
 	8: 0x3,
 }
+
+// Decode an entry encoded relative to another `Entry` from GrowingBytes.
 
 func DecodeStreamEntryRelativeEntry[NamespaceId, SubspaceId, PayloadDigest constraints.Ordered, ValueType constraints.Unsigned](
 	opts struct {
@@ -303,7 +323,188 @@ func EncodeEntryRelativeRange3d[NamespaceId, SubspaceId, PayloadDigest constrain
 	var encodedPath []byte
 
 	if !outer.PathRange.OpenEnd {
-		commonPrefixStart := CommonPrefix(entry.Path, outer.PathRange.Start)
+		commonPrefixStart, err := CommonPrefix(entry.Path, outer.PathRange.Start)
+		if err != nil {
+			panic(err)
+		}
+		commonPrefixEnd, err := CommonPrefix(entry.Path, outer.PathRange.End)
+		if err != nil {
+			panic(err)
+		}
 
+		if len(commonPrefixStart) >= len(commonPrefixEnd) {
+			encodePathRelativeToStartFlag = 0x40
+			encodedPath = EncodeRelativePath[ValueType](opts.PathScheme, entry.Path, outer.PathRange.Start)
+
+		} else {
+			encodePathRelativeToStartFlag = 0x0
+			encodedPath = EncodeRelativePath[ValueType](opts.PathScheme, entry.Path, outer.PathRange.End)
+		}
+	} else {
+		encodePathRelativeToStartFlag = 0x40
+		encodedPath = EncodeRelativePath[ValueType](opts.PathScheme, entry.Path, outer.PathRange.Start)
+	}
+	var applyTimeDiffWithStartOrEnd int
+	if timeDiff == AbsDiffuint64(entry.Timestamp, outer.TimeRange.Start) {
+		applyTimeDiffWithStartOrEnd = 0x20
+
+	} else {
+		applyTimeDiffWithStartOrEnd = 0x0
+	}
+
+	var addOrSubtractTimeDiffFlag int
+	if !outer.TimeRange.OpenEnd {
+		if (applyTimeDiffWithStartOrEnd == 0x20 && entry.Timestamp >= outer.TimeRange.Start) ||
+			(applyTimeDiffWithStartOrEnd == 0x0 &&
+				entry.Timestamp >= outer.TimeRange.End) {
+			addOrSubtractTimeDiffFlag = 0x10
+		} else {
+			addOrSubtractTimeDiffFlag = 0x0
+		}
+	} else {
+		if applyTimeDiffWithStartOrEnd == 0x20 &&
+			entry.Timestamp >= outer.TimeRange.Start {
+			addOrSubtractTimeDiffFlag = 0x10
+		} else {
+			addOrSubtractTimeDiffFlag = 0x0
+		}
+	}
+
+	var timeDiffCompactWidthFlag = CompactWidthEndMasks[GetWidthMax64Int(timeDiff)] << 2
+	var payloadLengthFlag = CompactWidthEndMasks[GetWidthMax64Int(entry.Payload_length)]
+
+	var header = encodedSubspaceIdFlag | encodePathRelativeToStartFlag |
+		applyTimeDiffWithStartOrEnd | addOrSubtractTimeDiffFlag |
+		timeDiffCompactWidthFlag | payloadLengthFlag
+
+	var encodedTimeDiff = EncodeIntMax64(timeDiff)
+
+	var encodedPayloadLength = EncodeIntMax64(entry.Payload_length)
+
+	var encodedPayloadDigest = opts.EncodePayloadDigest(entry.Payload_digest)
+
+	return append(
+		append(
+			append(
+				append(
+					append(
+						[]byte{byte(header)},
+						encodedSubspace...),
+					encodedPath...),
+				encodedTimeDiff...),
+			encodedPayloadLength...),
+		encodedPayloadDigest...)
+}
+
+func decodeStreamEntryRelativeRange3d[NamespaceId, SubspaceId, PayloadDigest constraints.Ordered, ValueType constraints.Unsigned](
+	opts struct {
+		DecodeStreamSubspace      func(bytes *GrowingBytes) chan SubspaceId
+		DecodeStreamPayloadDigest func(bytes *GrowingBytes) chan PayloadDigest
+		PathScheme                types.PathParams[ValueType]
+	},
+	bytes *GrowingBytes,
+	outer types.Range3d[SubspaceId],
+	namespaceId NamespaceId,
+) chan types.Entry[NamespaceId, SubspaceId, PayloadDigest] {
+	resultChan := make(chan types.Entry[NamespaceId, SubspaceId, PayloadDigest], 1)
+
+	go func() {
+
+		firstByte := bytes.NextAbsolute(1)
+		header := firstByte[0]
+
+		isSubspaceEncoded := (header & 0x80) == 0x80
+		isPathEncodedRelativeToStart := (header & 0x40) == 0x40
+		isTimeDiffCombinedWithStart := (header & 0x20) == 0x20
+		addOrSubtractTimedDiff := (header & 0x10) == 0x10
+		timeDiffCompactWidth := math.Pow(2, float64((header&0xc)>>2))
+		payloadLengthCompactWidth := math.Pow(2, float64(header&0x3))
+
+		var subspaceId SubspaceId
+
+		bytes.Prune(1)
+
+		if isSubspaceEncoded {
+			subspaceId = <-opts.DecodeStreamSubspace(bytes)
+		} else {
+			subspaceId = outer.SubspaceRange.Start
+		}
+
+		var path types.Path
+
+		if !isPathEncodedRelativeToStart {
+			if outer.PathRange.OpenEnd {
+				panic("The path cannot be encoded relative to an open end.")
+			}
+			path = DecodeRelPathStream[ValueType](opts.PathScheme, bytes, outer.PathRange.End)
+		} else {
+			path = DecodeRelPathStream[ValueType](opts.PathScheme, bytes, outer.PathRange.Start)
+		}
+
+		timeDiffBytes := bytes.NextAbsolute(int(timeDiffCompactWidth))
+		timeDiff, err := DecodeIntMax64(timeDiffBytes[:int(timeDiffCompactWidth)])
+		if err != nil {
+			panic(err)
+		}
+
+		bytes.Prune(int(timeDiffCompactWidth))
+
+		var timestamp uint64
+
+		if isTimeDiffCombinedWithStart && addOrSubtractTimedDiff {
+			timestamp = outer.TimeRange.Start + timeDiff
+		} else if isTimeDiffCombinedWithStart && !addOrSubtractTimedDiff {
+			timestamp = outer.TimeRange.Start - timeDiff
+		} else if !isTimeDiffCombinedWithStart && addOrSubtractTimedDiff {
+			if outer.TimeRange.OpenEnd {
+				panic("Can't apply time diff to an open end")
+			}
+
+			timestamp = outer.TimeRange.End + timeDiff
+		} else {
+			if outer.TimeRange.OpenEnd {
+				panic("Can't apply time diff to an open end")
+			}
+
+			timestamp = outer.TimeRange.End - timeDiff
+		}
+
+		payloadLengthBytes := bytes.NextAbsolute(int(payloadLengthCompactWidth))
+		payloadLength, err := DecodeIntMax64(payloadLengthBytes[:int(payloadLengthCompactWidth)])
+		if err != nil {
+			panic(err)
+		}
+
+		bytes.Prune(int(payloadLengthCompactWidth))
+
+		payloadDigest := <-opts.DecodeStreamPayloadDigest(bytes)
+
+		resultChan <- types.Entry[NamespaceId, SubspaceId, PayloadDigest]{
+			Namespace_id:   namespaceId,
+			Subspace_id:    subspaceId,
+			Path:           path,
+			Timestamp:      timestamp,
+			Payload_length: payloadLength,
+			Payload_digest: payloadDigest,
+		}
+
+	}()
+
+	return resultChan
+
+}
+
+func DefaultEntry[NamespaceId, SubspaceId, PayloadDigest constraints.Ordered, ValueType constraints.Unsigned](
+	defaultNamespace NamespaceId,
+	defaultSubspace SubspaceId,
+	defaultPayloadDigest PayloadDigest,
+) types.Entry[NamespaceId, SubspaceId, PayloadDigest] {
+	return types.Entry[NamespaceId, SubspaceId, PayloadDigest]{
+		Namespace_id:   defaultNamespace,
+		Subspace_id:    defaultSubspace,
+		Path:           types.Path{},
+		Timestamp:      0,
+		Payload_length: 0,
+		Payload_digest: defaultPayloadDigest,
 	}
 }
