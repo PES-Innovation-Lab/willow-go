f05fdaab6ee916c3728d4a65c93d1e311bccf7f9
samarth-clone <samarthbhaty@gmail.com>
2024-07-17 16:39:51 +0530

    🤐

--- pkg/data_model/store/store_test.go
+++ pkg/data_model/store/store_test.go
@@ -1,17 +1,34 @@
 package store
 
 import (
+	"log"
 	"testing"
 
+	entrydriver "github.com/PES-Innovation-Lab/willow-go/pkg/data_model/entry_driver"
 	"github.com/PES-Innovation-Lab/willow-go/pkg/data_model/kv_driver"
+	payloadDriver "github.com/PES-Innovation-Lab/willow-go/pkg/data_model/payload_kv_driver"
+	"github.com/cockroachdb/pebble"
 )
 
 func InitStorage() *Store[uint64, uint64, uint8, any, string] {
-	return &Store[uint64, uint64, uint8, any, string]{
-		Schemes: StoreSchemes,
-		EntryDriver: ,
+
+	db, err := pebble.Open("demo", &pebble.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	Kvstore := kv_driver.KvDriver{Db: db}
+	PayloadReferenceCounter := payloadDriver.PayloadReferenceCounter{
+		Store: Kvstore,
 	}
 
+	entryDriver := entrydriver.EntryDriver[uint64, uint64, uint64]{
+		PayloadReferenceCounter: PayloadReferenceCounter,
+		Opts:                    struct{},
+	}
+
+	return &Store[uint64, uint64, uint8, any, string]{
+		Schemes: StoreSchemes,
+	}
 }
 
 func TestSet(t *testing.T) {
