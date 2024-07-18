6851ae876cbaa4354e2d41b95439d8df8a1e3dc6
samarth-clone <samarthbhaty@gmail.com>
2024-07-18 23:58:48 +0530

    Trying Cobra

--- PinaGolada/main.go
+++ PinaGolada/main.go
@@ -1,1 +1,24 @@
 package PinaGolada
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "pinalada",
+		Short: "An instantiation of the WillowGo protocol!",
+		Long:  "Willow is p2p protocol for file storing and sharing. Pinagolada is an instantiation of the protocll with specific parameters",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Niggesh")
+		},
+	}
+
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
